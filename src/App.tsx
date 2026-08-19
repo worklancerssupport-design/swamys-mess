12b55a1f1b70d76d396ef54e250063ad9b63bb1a
Pratyush N K <pratyushnk01@gmail.com>
2026-08-19 18:45:16 +0530

    github method init setup

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: "system-ui", textAlign: "center", marginTop: "4rem" }}>
+      <h1>Swamy's Mess</h1>
+      <p>Mess & Catering Services</p>
+    </div>
+  );
+}
